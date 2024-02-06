22500a1c6e8058777d38c28d3b5315e940aad3ba
Sowndaryakp <sonalshetty.1999@gmail.com>
2024-02-06 06:18:46 +0530

    resolving lombok

---
+++ Project1(CRUD)/Backend/crud/src/main/java/com/example/crud/model/Todo.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private boolean completed;
+
+    
+    // No need to manually write constructors, getters, and setters
+}
