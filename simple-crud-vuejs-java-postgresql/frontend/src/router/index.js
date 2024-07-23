4f663b23c045801786f05d34cdb0af1dfeec80f0
Sowndaryakp <sonalshetty.1999@gmail.com>
2024-07-23 22:30:06 +0530

    working on ems

--- simple-crud-vuejs-java-postgresql/frontend/src/router/index.js
+++ simple-crud-vuejs-java-postgresql/frontend/src/router/index.js
@@ -1,25 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
 import ViewPatients from '../views/ViewPatients.vue'
 
-
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'Login', // You can set Login as the default route instead
-      component: Login
+      name: 'home',
+      component: ViewPatients
     },
     {
-      path: '/login',
-      redirect: '/' // Redirect to login if user tries to access /login
+      path: '/add',
+      name: 'add',
+      component: () => import('../views/AddPatient.vue')
     },
     {
-      path: '/view-patients', // Define the path for the ViewPatients component
-      name: 'ViewPatients',
-      component: ViewPatients,
-      meta: { requiresAuth: true } // Optionally, add meta data to indicate that authentication is required for this route
+      path: '/edit/:id',
+      name: 'edit',
+      component: () => import('../views/UpdatePatient.vue')
     }
   ]
 })
